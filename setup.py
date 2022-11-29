204c6b559cda48de7e179a36699420e6abe0f357
hantmac <hantmac@outlook.com>
2022-11-29 21:43:54 +0800

    fix set

--- setup.py
+++ setup.py
@@ -19,4 +19,15 @@ setup(
     install_requires=[
         "dbt-core~=1.3.0",
     ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8,<=3.10",
 )
