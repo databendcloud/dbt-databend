c4fc9e0591e4d583d00e3a1c21971b143031ed97
hantmac <hantmac@outlook.com>
2024-05-30 16:19:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,12 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-common>=0.1.0a1,<2.0",
-        "dbt-adapters>=0.1.0a1,<2.0",
+        "dbt-common>=1.0.4,<2.0",
+        "dbt-adapters>=1.1.1,<2.0",
+        # add dbt-core to ensure backwards compatibility of installation, this is not a functional dependency
+        "dbt-core>=1.8.0",
+        # installed via dbt-core but referenced directly; don't pin to avoid version conflicts with dbt-core
+        "agate",
         "databend-sqlalchemy~=0.3.2",
         "agate",
     ],
