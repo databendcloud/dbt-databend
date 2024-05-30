fd7cef333744a7761f41efb0d8b204961e9ba431
hantmac <hantmac@outlook.com>
2024-05-30 10:17:50 +0800

    add init file and some tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-databend-cloud"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.8.0b1"
+package_version = "1.8.1"
 description = """The Databend adapter plugin for dbt"""
 
 setup(
